0fb5711e6d2fad9db9adeae23b91fd1643cacc4d
DinoDotRawr <dushravifuture@gmail.com>
2025-10-16 22:05:58 -0400

    Update RobotConstants.java
    added elevator variables?
    change if needed

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -22,37 +22,27 @@ public class RobotConstants {
     public static double robotSpeed = 0.5;
     public static boolean cruiseControl = false; // fasle=off true=on
 
-    // shooting/loading
-    public static final double loaderPower = 0.4;
-    public static double launcherLimit = 0.8;
-    public static double loaderOutput = 0;
-    public static boolean shooterTogglePressed = false;
-    public static boolean shooterOutput = false;
-    public static boolean hasBall = false;
-    // 0.5 not high enough
-    // 0.8 most consistant
+    // elevator (all values in inches)
+    public static double elevatorHeight;
+    public static final double Level1=24;
+    public static final double Level2=24+17;
+    public static final double Level3=24+17*2;
+    public static final double humanPlayer=35;
 
-    // not really constansts but whatever
+
+    // manipulaor
+
     // inputs
     public static double rightTrigger;
     public static double leftStick;{if (Math.abs(leftStick) <= .05) {leftStick=0;} else{leftStick=leftStick;}}
-    public static double rightStick;{if (Math.abs(rightStick) <= .05) {rightStick=0;} else{rightStick=rightStick;}}
+    public static double rightStick;
     public static Boolean leftBumper;
     public static Boolean rightBumper;
     public static double leftTrigger;
     public static boolean bButton;
     public static boolean xButton;
     public static boolean aButton;
-    public static boolean beamSensor; // true = connects false = somthing blocking
 
-    // loader code (prev version)
-    // public static double intakeContactRemovalTime = 0.0;
-    // public static Boolean intakeWaitVar = false;
-    // public static Boolean intakeRun = false;
-    // public static double intakeRunTime = 0.0;
-    // public static Boolean loaderControllerDisconnect = false;
 
-    // public static double rollbackFactor = .25;
-    // public static double rollbackMax = 2;
 
 }
