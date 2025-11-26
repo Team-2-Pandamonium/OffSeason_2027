e65b9fbe8da68a13cc989814e79fe88f714b7a39
DinoDotRawr <dushravifuture@gmail.com>
2025-11-25 20:50:45 -0500

    U sure ur the fucking goat?

--- src/main/java/frc/robot/commands/Manipulator.java
+++ src/main/java/frc/robot/commands/Manipulator.java
@@ -4,42 +4,4 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Manipulator extends SubsystemBase {
 
-    /**
-     * 
-     * @param drvRot
-     * @return <b>linVel<b> (inches)
-     */
-    public static double drvRotLinVel(double drvMotRot){
-        double wheelRot=drvMotRot/8.46;
-        return wheelRot*(Math.PI*6);
-    }
-
-    /**
-     * 
-     * @param linVel
-     * @return <b>drvRot<b>
-     */
-
-    /**
-     * 
-     * @param degrees to turn
-     * @param seconds for degrees to turn
-     * @return turning speed of both motors, one negative and one positive, for drivetrain
-     */
-    public static double degAndSecsToDrvSpeed(double degrees, double seconds) {
-        return 13.5*(degrees/seconds)*(Math.PI/180);
-        // (Width of the robot: 27, divided by 2, since 2 seperate sides) times (degrees/sec)*(pi/180), which is the equation for radians per sec
-    }
-
-
-    /**
-     * @apiNote divide by circumference (diameter=4), and multiply by gear ratios (4)
-     * @param LinVel
-     * @return <b>manRot<b> (rot/sec)
-     */
-    public static double LinVeltoManRot(double LinVel){
-        double wheelRot=LinVel/(Math.PI*4);
-        return wheelRot*4;
-    }
-    
 }
